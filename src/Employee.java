b3bde4a0a5f0eb29c9983ebb4f8ace2f04875c8c
GMFAD <gmfad69@gmail.com>
2026-01-13 01:53:38 +0600

    2.2.8 half: Solved negetive number issue

--- src/Employee.java
+++ src/Employee.java
@@ -1,8 +1,18 @@
 public class Employee {
-    public int baseSalary;
+    private int baseSalary;
     public int hourlyRate;
 
     public int calculateWage (int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be less than or equal to 0");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
    static void main() {
        var employee = new Employee();
-       employee.baseSalary = 50_000;
+       employee.setBaseSalary(343434);
        employee.hourlyRate = 20;
        int wage = employee.calculateWage(10);
        System.out.println(wage);
