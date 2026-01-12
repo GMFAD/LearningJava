357370a7ba27100672873a5c94868641d7067203
GMFAD <gmfad69@gmail.com>
2026-01-12 16:43:44 +0600

    2.2.7: Encapsulation done

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
 public class Main {
    static void main() {
-       int baseSalary = 50_000;
-       int overtime = 10;
-       int hourly = 20;
-
-       int wage = calculateWage(baseSalary, overtime, hourly);
+       var employee = new Employee();
+       employee.baseSalary = 50_000;
+       employee.hourlyRate = 20;
+       int wage = employee.calculateWage(10);
        System.out.println(wage);
-    }
-
-    public static int calculateWage(
-            int baseSalary,
-            int overtime,
-            int hourly
-    ) {
-       return baseSalary + (overtime * hourly);
     }
 }
