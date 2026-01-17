3ea9f59e36ef21344035b387184274e6e7304dec
GMFAD <gmfad69@gmail.com>
2026-01-17 20:15:21 +0600

    2.2.11: all of coupling is done

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
    static void main() {
-       var employee = new Employee();
-       employee.setBaseSalary(343434);
-       employee.setHourlyRate(20);
-       int wage = employee.calculateWage(10);
-       System.out.println(wage);
-       //employee.
+       Browser browser = new Browser();
+       //browser.
     }
 }
