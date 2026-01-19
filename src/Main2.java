3e5e4a8504f26ce6cf3c0df65b52ec715b83c743
GMFAD <gmfad69@gmail.com>
2026-01-19 20:21:41 +0600

    2.3.2: Inheritance done

---
+++ src/Main2.java
@@ -0,0 +1,7 @@
+public class Main2 {
+    public static void main(String[] args) {
+        var control = new UIControl();
+        control.disable();
+        System.out.println(control.isEnabled());
+    }
+}
