941ff30bf2af64e3cf28ec1fa9ce0b92235e65de
GMFAD <gmfad69@gmail.com>
2026-01-21 03:07:16 +0600

    clear

--- src/Main2.java
+++ src/Main2.java
@@ -1,7 +1,4 @@
 public class Main2 {
     public static void main(String[] args) {
-        var control = new TextBox();
-        control.
-        System.out.println(control.isEnabled());
     }
 }
