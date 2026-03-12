9e9bccd266ff436b1f6f493e0b7ec9728b7ab32f
GMFAD <gmfad69@gmail.com>
2026-03-13 03:45:55 +0600

    2.4.2.1: inheritance done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     static void main() {
-        var employee = new Employee(50_000, 20);
-        int wage = employee.calculateWage(10);
-        System.out.println(wage);
-        //employee.
+        var control = new UIControl();
+        control.disable();
+        System.out.println(control.isEnabled());
     }
 }
